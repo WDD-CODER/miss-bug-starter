051dd67ab63e62bfe4586c8dde34eea2f64d2edf
Your Name <dan.weibren@gmail.com>
2025-07-24 12:02:22 +0300

    feat(server): implement basic Express bug API with mock data
    - Created RESTful routes to list, save, fetch, and remove bugs
    - Added mock bug list in memory with unique _id
    - Enabled save via query params using /api/bug/save

--- server.js
+++ server.js
@@ -1,4 +1,72 @@
 import express from 'express'
+import { makeId, readJsonFile } from './services/util.service.js'
+
 const app = express()
-app.get('/', (req, res) => res.send('Hello there'))
+
+// const bugs = readJsonFile('./data/bugs.json')
+const bugs = [
+    {
+        title: "Infinite Loop Detected",
+        severity: 44,
+        _id: "1NF1N1T3"
+    },
+    {
+        title: "Infinite Loop Detected",
+        severity: 4,
+        _id: "1NF1N1T3"
+    },
+    {
+        title: "Keyboard Not Found",
+        severity: 3,
+        _id: "K3YB0RD"
+    },
+    {
+        title: "404 Coffee Not Found",
+        severity: 2,
+        _id: "C0FF33"
+    },
+    {
+        title: "Unexpected Response",
+        severity: 1,
+        _id: "G0053"
+    }
+]
+
+app.get('/api/bug', (req, res) => { res.send(bugs) })
+
+
+app.get('/api/bug/save', (req, res) => {
+    const { title, severity, _id } = req.query
+    const bug = {
+        title,
+        severity:+severity
+    }
+    
+    if (_id) {
+        bug.id = _id
+        const idx = bugs.findIndex(bug => bug._id === _id)
+        bugs.splice(idx, 1, bug)
+    }
+    else {
+        bug.id = makeId()
+        bugs.push(bug)
+    }
+    res.send(bug)
+})
+
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const bugId = req.params.bugId
+    const bug = bugs.find(bug => bug._id === bugId)
+    res.send(bug)
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const bugId = req.params.bugId
+    const idx = bugs.findIndex(bug => bug._id === bugId)
+    bugs.splice(idx, 1)
+    res.send(`bug ${bugId} removed`)
+
+})
+
 app.listen(3030, () => console.log('Server ready at port 3030'))
--- services/util.service.js
+++ services/util.service.js
@@ -1,12 +1,8 @@
-export const utilService = {
-    makeId,
-    makeLorem,
-    getRandomIntInclusive,
-    loadFromStorage,
-    saveToStorage
-}
+import http from 'http'
+import https from 'https'
+import fs from 'fs'
 
-function makeId(length = 6) {
+export function makeId(length = 6) {
     var txt = ''
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
@@ -17,7 +13,7 @@ function makeId(length = 6) {
     return txt
 }
 
-function makeLorem(size = 100) {
+export function makeLorem(size = 100) {
     var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
     var txt = ''
     while (size > 0) {
@@ -27,19 +23,76 @@ function makeLorem(size = 100) {
     return txt
 }
 
-function getRandomIntInclusive(min, max) {
+export function getRandomIntInclusive(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
 
 
-function loadFromStorage(keyDB) {
+export function loadFromStorage(keyDB) {
     const val = localStorage.getItem(keyDB)
     return JSON.parse(val)
 }
 
-function saveToStorage(keyDB, val) {
+export function saveToStorage(keyDB, val) {
     const valStr = JSON.stringify(val)
     localStorage.setItem(keyDB, valStr)
 }
+
+export function readJsonFile(path) {
+    const str = fs.readFileSync(path, 'utf8')
+    const json = JSON.parse(str)
+    return json
+}
+
+export function writeJsonFile(path, json) {
+    const data = JSON.stringify(json, null, 4)
+
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, data, err => {
+            if (err) reject(err)
+            else resolve()
+        })
+    })
+}
+
+export function download(url, fileName) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(fileName)
+        https.get(url, content => {
+            content.pipe(file)
+            file.on('error', reject)
+            file.on('finish', () => {
+                file.close()
+                resolve()
+            })
+        })
+    })
+}
+
+
+export function httpGet(url) {
+    const protocol = url.startsWith('https') ? https : http
+    const options = {
+        method: 'GET',
+    }
+    return new Promise((resolve, reject) => {
+        const req = protocol.request(url, options, res => {
+            let data = ''
+
+            res.on('data', chunk => {
+                data += chunk
+            })
+
+            res.on('end', () => {
+                resolve(data)
+            })
+        })
+        req.on('error', err => {
+            reject(err)
+        })
+        req.end()
+    })
+}
+
